20a703f6390cc23a0411f0058d26931c714e0b6a
dhlee <dhlee@meissa.ai>
2022-02-25 17:07:53 +0900

    Build diff-rs-sfm module

---
+++ src/build/CMakeFiles/diff_RS_SfM.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dhlee/meissa/RS-aware-differential-SfM/src/CustomDeepFlow.cc" "/home/dhlee/meissa/RS-aware-differential-SfM/src/build/CMakeFiles/diff_RS_SfM.dir/CustomDeepFlow.cc.o"
+  "/home/dhlee/meissa/RS-aware-differential-SfM/src/camera.cc" "/home/dhlee/meissa/RS-aware-differential-SfM/src/build/CMakeFiles/diff_RS_SfM.dir/camera.cc.o"
+  "/home/dhlee/meissa/RS-aware-differential-SfM/src/deepflow.cpp" "/home/dhlee/meissa/RS-aware-differential-SfM/src/build/CMakeFiles/diff_RS_SfM.dir/deepflow.cpp.o"
+  "/home/dhlee/meissa/RS-aware-differential-SfM/src/errorMeasure.cpp" "/home/dhlee/meissa/RS-aware-differential-SfM/src/build/CMakeFiles/diff_RS_SfM.dir/errorMeasure.cpp.o"
+  "/home/dhlee/meissa/RS-aware-differential-SfM/src/main.cc" "/home/dhlee/meissa/RS-aware-differential-SfM/src/build/CMakeFiles/diff_RS_SfM.dir/main.cc.o"
+  "/home/dhlee/meissa/RS-aware-differential-SfM/src/minimal.cc" "/home/dhlee/meissa/RS-aware-differential-SfM/src/build/CMakeFiles/diff_RS_SfM.dir/minimal.cc.o"
+  "/home/dhlee/meissa/RS-aware-differential-SfM/src/nonlinearRefinement.cc" "/home/dhlee/meissa/RS-aware-differential-SfM/src/build/CMakeFiles/diff_RS_SfM.dir/nonlinearRefinement.cc.o"
+  "/home/dhlee/meissa/RS-aware-differential-SfM/src/rsframe.cc" "/home/dhlee/meissa/RS-aware-differential-SfM/src/build/CMakeFiles/diff_RS_SfM.dir/rsframe.cc.o"
+  "/home/dhlee/meissa/RS-aware-differential-SfM/src/scanline.cc" "/home/dhlee/meissa/RS-aware-differential-SfM/src/build/CMakeFiles/diff_RS_SfM.dir/scanline.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CERES_EXPORT_INTERNAL_SYMBOLS"
+  "CERES_GFLAGS_NAMESPACE="
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../gflags_shared"
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/CMakeFiles/diff_RS_SfM.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/diff_RS_SfM.dir/CustomDeepFlow.cc.o"
+  "CMakeFiles/diff_RS_SfM.dir/main.cc.o"
+  "CMakeFiles/diff_RS_SfM.dir/camera.cc.o"
+  "CMakeFiles/diff_RS_SfM.dir/rsframe.cc.o"
+  "CMakeFiles/diff_RS_SfM.dir/scanline.cc.o"
+  "CMakeFiles/diff_RS_SfM.dir/nonlinearRefinement.cc.o"
+  "CMakeFiles/diff_RS_SfM.dir/minimal.cc.o"
+  "CMakeFiles/diff_RS_SfM.dir/errorMeasure.cpp.o"
+  "CMakeFiles/diff_RS_SfM.dir/deepflow.cpp.o"
+  "diff_RS_SfM.pdb"
+  "diff_RS_SfM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/diff_RS_SfM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
