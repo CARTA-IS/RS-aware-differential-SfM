b1284cda2617d9802f71f954572369cacf2530a6
dhlee <dhlee@meissa.ai>
2022-03-30 16:53:00 +0900

    Add optical flow to direction

--- deepmatching_1.2.2/deepmatching_1.2.2_c++/command.sh
+++ deepmatching_1.2.2/deepmatching_1.2.2_c++/command.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
-./deepmatching $PWD/test_result/resize_578790.JPG $PWD/test_result/resize_578791.JPG -nt 24 -out output_578790.txt
-./deepmatching $PWD/test_result/resize_578791.JPG $PWD/test_result/resize_578792.JPG -nt 24 -out output_578791.txt
-./deepmatching $PWD/test_result/resize_578792.JPG $PWD/test_result/resize_578793.JPG -nt 24 -out output_578792.txt
-./deepmatching $PWD/test_result/resize_578793.JPG $PWD/test_result/resize_578794.JPG -nt 24 -out output_578793.txt
-./deepmatching $PWD/test_result/resize_578794.JPG $PWD/test_result/resize_578795.JPG -nt 24 -out output_578794.txt
-./deepmatching $PWD/test_result/resize_578795.JPG $PWD/test_result/resize_578796.JPG -nt 24 -out output_578795.txt
-./deepmatching $PWD/test_result/resize_578796.JPG $PWD/test_result/resize_578797.JPG -nt 24 -out output_578796.txt
-./deepmatching $PWD/test_result/resize_578797.JPG $PWD/test_result/resize_578798.JPG -nt 24 -out output_578797.txt
-./deepmatching $PWD/test_result/resize_578798.JPG $PWD/test_result/resize_578799.JPG -nt 24 -out output_578798.txt
-./deepmatching $PWD/test_result/resize_578799.JPG $PWD/test_result/resize_578800.JPG -nt 24 -out output_578799.txt
-./deepmatching $PWD/test_result/resize_578800.JPG $PWD/test_result/resize_578801.JPG -nt 24 -out output_578800.txt
-./deepmatching $PWD/test_result/resize_578801.JPG $PWD/test_result/resize_578802.JPG -nt 24 -out output_578801.txt
-./deepmatching $PWD/test_result/resize_578802.JPG $PWD/test_result/resize_578803.JPG -nt 24 -out output_578802.txt
-./deepmatching $PWD/test_result/resize_578803.JPG $PWD/test_result/resize_578804.JPG -nt 24 -out output_578803.txt
-./deepmatching $PWD/test_result/resize_578804.JPG $PWD/test_result/resize_578805.JPG -nt 24 -out output_578804.txt
-./deepmatching $PWD/test_result/resize_578805.JPG $PWD/test_result/resize_578806.JPG -nt 24 -out output_578805.txt
-./deepmatching $PWD/test_result/resize_578806.JPG $PWD/test_result/resize_578807.JPG -nt 24 -out output_578806.txt
-./deepmatching $PWD/test_result/resize_578807.JPG $PWD/test_result/resize_578808.JPG -nt 24 -out output_578807.txt
-./deepmatching $PWD/test_result/resize_578808.JPG $PWD/test_result/resize_578809.JPG -nt 24 -out output_578808.txt
+
+./deepmatching $PWD/resize_real/resize_100_0024_0001.JPG $PWD/resize_real/resize_100_0024_0002.JPG -nt 24 -out output_100_0024_0001.txt
+./deepmatching $PWD/resize_real/resize_100_0024_0002.JPG $PWD/resize_real/resize_100_0024_0003.JPG -nt 24 -out output_100_0024_0002.txt
+./deepmatching $PWD/resize_real/resize_100_0024_0003.JPG $PWD/resize_real/resize_100_0024_0004.JPG -nt 24 -out output_100_0024_0003.txt
+./deepmatching $PWD/resize_real/resize_100_0024_0004.JPG $PWD/resize_real/resize_100_0024_0005.JPG -nt 24 -out output_100_0024_0004.txt
+./deepmatching $PWD/resize_real/resize_100_0024_0005.JPG $PWD/resize_real/resize_100_0024_0006.JPG -nt 24 -out output_100_0024_0005.txt
+./deepmatching $PWD/resize_real/resize_100_0024_0006.JPG $PWD/resize_real/resize_100_0024_0007.JPG -nt 24 -out output_100_0024_0006.txt
+./deepmatching $PWD/resize_real/resize_100_0024_0007.JPG $PWD/resize_real/resize_100_0024_0008.JPG -nt 24 -out output_100_0024_0007.txt
+./deepmatching $PWD/resize_real/resize_100_0024_0008.JPG $PWD/resize_real/resize_100_0024_0009.JPG -nt 24 -out output_100_0024_0008.txt
+./deepmatching $PWD/resize_real/resize_100_0024_0009.JPG $PWD/resize_real/resize_100_0024_0010.JPG -nt 24 -out output_100_0024_0009.txt
+
+# ./deepmatching $PWD/resize_real/resize_MAX_0001.JPG $PWD/resize_real/resize_MAX_0002.JPG -nt 24 -out output_MAX_0001.txt
+# ./deepmatching $PWD/resize_real/resize_MAX_0002.JPG $PWD/resize_real/resize_MAX_0003.JPG -nt 24 -out output_MAX_0002.txt
+# ./deepmatching $PWD/resize_real/resize_MAX_0003.JPG $PWD/resize_real/resize_MAX_0004.JPG -nt 24 -out output_MAX_0003.txt
+# ./deepmatching $PWD/resize_real/resize_MAX_0004.JPG $PWD/resize_real/resize_MAX_0005.JPG -nt 24 -out output_MAX_0004.txt
+# ./deepmatching $PWD/resize_real/resize_MAX_0005.JPG $PWD/resize_real/resize_MAX_0006.JPG -nt 24 -out output_MAX_0005.txt
+# ./deepmatching $PWD/resize_real/resize_MAX_0006.JPG $PWD/resize_real/resize_MAX_0007.JPG -nt 24 -out output_MAX_0006.txt
+# ./deepmatching $PWD/resize_real/resize_MAX_0007.JPG $PWD/resize_real/resize_MAX_0008.JPG -nt 24 -out output_MAX_0007.txt
+# ./deepmatching $PWD/resize_real/resize_MAX_0008.JPG $PWD/resize_real/resize_MAX_0009.JPG -nt 24 -out output_MAX_0008.txt
+# ./deepmatching $PWD/resize_real/resize_MAX_0009.JPG $PWD/resize_real/resize_MAX_0010.JPG -nt 24 -out output_MAX_0009.txt
+# ./deepmatching $PWD/resize_real/resize_MAX_0010.JPG $PWD/resize_real/resize_MAX_0011.JPG -nt 24 -out output_MAX_0010.txt
+
+# ./deepmatching $PWD/resize_real/resize_578790.JPG $PWD/resize_real/resize_578791.JPG -nt 24 -out output_578790.txt
+# ./deepmatching $PWD/resize_real/resize_578791.JPG $PWD/resize_real/resize_578792.JPG -nt 24 -out output_578791.txt
+# ./deepmatching $PWD/resize_real/resize_578792.JPG $PWD/resize_real/resize_578793.JPG -nt 24 -out output_578792.txt
+# ./deepmatching $PWD/resize_real/resize_578793.JPG $PWD/resize_real/resize_578794.JPG -nt 24 -out output_578793.txt
+# ./deepmatching $PWD/resize_real/resize_578794.JPG $PWD/resize_real/resize_578795.JPG -nt 24 -out output_578794.txt
+# ./deepmatching $PWD/resize_real/resize_578795.JPG $PWD/resize_real/resize_578796.JPG -nt 24 -out output_578795.txt
+# ./deepmatching $PWD/resize_real/resize_578796.JPG $PWD/resize_real/resize_578797.JPG -nt 24 -out output_578796.txt
+# ./deepmatching $PWD/resize_real/resize_578797.JPG $PWD/resize_real/resize_578798.JPG -nt 24 -out output_578797.txt
+# ./deepmatching $PWD/resize_real/resize_578798.JPG $PWD/resize_real/resize_578799.JPG -nt 24 -out output_578798.txt
+# ./deepmatching $PWD/resize_real/resize_578799.JPG $PWD/resize_real/resize_578800.JPG -nt 24 -out output_578799.txt
+# ./deepmatching $PWD/resize_real/resize_578800.JPG $PWD/resize_real/resize_578801.JPG -nt 24 -out output_578800.txt
+# ./deepmatching $PWD/resize_real/resize_578801.JPG $PWD/resize_real/resize_578802.JPG -nt 24 -out output_578801.txt
+# ./deepmatching $PWD/resize_real/resize_578802.JPG $PWD/resize_real/resize_578803.JPG -nt 24 -out output_578802.txt
+# ./deepmatching $PWD/resize_real/resize_578803.JPG $PWD/resize_real/resize_578804.JPG -nt 24 -out output_578803.txt
+# ./deepmatching $PWD/resize_real/resize_578804.JPG $PWD/resize_real/resize_578805.JPG -nt 24 -out output_578804.txt
+# ./deepmatching $PWD/resize_real/resize_578805.JPG $PWD/resize_real/resize_578806.JPG -nt 24 -out output_578805.txt
+# ./deepmatching $PWD/resize_real/resize_578806.JPG $PWD/resize_real/resize_578807.JPG -nt 24 -out output_578806.txt
+# ./deepmatching $PWD/resize_real/resize_578807.JPG $PWD/resize_real/resize_578808.JPG -nt 24 -out output_578807.txt
+# ./deepmatching $PWD/resize_real/resize_578808.JPG $PWD/resize_real/resize_578809.JPG -nt 24 -out output_578808.txt
 # ./deepmatching $PWD/test_result/resize_578809.JPG $PWD/test_result/resize_578810.JPG -nt 24 -out output_578809.txt
+
+# i*1
--- deepmatching_1.2.2/deepmatching_1.2.2_c++/optical_flow_viz.py
+++ deepmatching_1.2.2/deepmatching_1.2.2_c++/optical_flow_viz.py
@@ -2,95 +2,26 @@ import os
 import cv2
 import sys
 import argparse
+from calc_angle import angle_to
 
 # bring output.txt to make (u,v)
 parser = argparse.ArgumentParser()
-parser.add_argument('-img', type=str, 
-                    help='an image for optical flow visualization')
+parser.add_argument('-dir', type=str, default="resize_real", 
+                    help='img dir for optical flow visualization')
+parser.add_argument('-num_imgs', type=int, default=0,
+                    help='choose number of imgs used for visualization')                    
 args = parser.parse_args()
 
 # for one image
-"""
-with open("output.txt", "r") as fp:
-    num_lines = sum(1 for line in fp)
-    print("Total lines:", num_lines)
-
-f = open("output.txt", "r")
-u = 0
-v = 0
-for i in range(num_lines):
-    line = f.readline()
-    if not line:
-        break
-    temp = []
-    count = 0
-    for pixel in line.split():
-        temp.append(int(pixel))
-        count += 1
-        if count >= 4:
-            break
-    u += temp[2] - temp[0]
-    v += temp[3] - temp[1]
-
-u /= num_lines
-v /= num_lines
-
-print(u, v)
-
-f.close()
-
-path = r'/home/dhlee/meissa/RS-aware-differential-SfM/deepmatching_1.2.2/deepmatching_1.2.2_c++/' + args.img
-
-# Reading an image in default mode
-image = cv2.imread(path)
-height, width, channels = image.shape
-
-# Window name in which image is displayed
-window_name = 'Image'
-
-# Start coordinate, here (0, 0)
-# represents the top left corner of image
-if (u > 0 and v > 0):
-    start_point = (0, height)
-    end_point = (int(u), height - int(v))
-elif (u > 0 and v <= 0):
-    start_point = (0, height)
-    end_point = (int(u), -1*int(v))
-elif (u <= 0 and v > 0):
-    start_point = (width, height)
-    end_point = (width - int(u), height - int(v))
-elif (u <= 0 and v <= 0): 
-    start_point = (width, 0)
-    end_point = (width + int(u), -1*int(v))
-
-# Green color in BGR
-color = (0, 255, 0)
-
-# Line thickness of 9 px
-thickness = 9
-
-# Using cv2.arrowedLine() method
-# Draw a diagonal green arrow line
-# with thickness of 9 px
-image = cv2.arrowedLine(image, start_point, end_point,
-									color, thickness)
-
-cv2.imwrite('arrow_image.png', image)
-"""
-
-# for several images
-img_list = os.listdir(r'/home/dhlee/meissa/RS-aware-differential-SfM/deepmatching_1.2.2/deepmatching_1.2.2_c++/test_result')
-img_list.sort()
-for i in range(len(img_list) - 1):
-    # print(img_list[i][7:13])
-    with open(f"output_{img_list[i][7:13]}.txt", "r") as fp:
+def one_image_visual():
+    with open("output.txt", "r") as fp:
         num_lines = sum(1 for line in fp)
-        print(f"output_{img_list[i][7:13]}.txt total lines:", num_lines)
+        print("Total lines:", num_lines)
 
-    f = open(f"output_{img_list[i][7:13]}.txt", "r")
+    f = open("output.txt", "r")
     u = 0
     v = 0
-    for j in range(num_lines):
+    for i in range(num_lines):
         line = f.readline()
         if not line:
             break
@@ -108,13 +39,20 @@ for i in range(len(img_list) - 1):
     v /= num_lines
 
     print(u, v)
+
     f.close()
 
-    print(img_list[i])
-    image = cv2.imread(r'/home/dhlee/meissa/RS-aware-differential-SfM/deepmatching_1.2.2/deepmatching_1.2.2_c++/test_result/' + img_list[i])
+    path = r'/home/dhlee/meissa/RS-aware-differential-SfM/deepmatching_1.2.2/deepmatching_1.2.2_c++/' + args.img
+
+    # Reading an image in default mode
+    image = cv2.imread(path)
     height, width, channels = image.shape
+
+    # Window name in which image is displayed
     window_name = 'Image'
 
+    # Start coordinate, here (0, 0)
+    # represents the top left corner of image
     if (u > 0 and v > 0):
         start_point = (0, height)
         end_point = (int(u), height - int(v))
@@ -128,11 +66,84 @@ for i in range(len(img_list) - 1):
         start_point = (width, 0)
         end_point = (width + int(u), -1*int(v))
 
-
+    # Green color in BGR
     color = (0, 255, 0)
+
+    # Line thickness of 9 px
     thickness = 9
+
+    # Using cv2.arrowedLine() method
+    # Draw a diagonal green arrow line
+    # with thickness of 9 px
     image = cv2.arrowedLine(image, start_point, end_point,
                                         color, thickness)
-    cv2.imwrite(f'arrow_{img_list[i][7:13]}.JPG', image)
+
+    cv2.imwrite('arrow_image.png', image)
 
 
+# for several images
+def multi_image_visual(dir, num_imgs):
+    img_list = os.listdir(f'/home/dhlee/meissa/RS-aware-differential-SfM/deepmatching_1.2.2/deepmatching_1.2.2_c++/{dir}')
+    img_list.sort()
+    if not num_imgs:
+        num_imgs = len(img_list)
+    for i in range(num_imgs - 1):
+        temp = img_list[i].replace("resize_", "")
+        name = temp.replace(".JPG", "")
+        with open(f"output_{name}.txt", "r") as f:
+            num_lines = sum(1 for line in f)
+            # print(f"{img_list[i]}\noutput_{name}.txt\ntotal lines: {num_lines}")
+
+        output_file = open(f"output_{name}.txt", "r")
+        u = 0
+        v = 0
+        for j in range(num_lines):
+            line = output_file.readline()
+            if not line:
+                break
+            temp = []
+            count = 0
+            for pixel in line.split():
+                temp.append(int(pixel))
+                count += 1
+                if count >= 4:
+                    break
+            u += temp[2] - temp[0]
+            v += temp[3] - temp[1]
+
+        u /= num_lines
+        v /= num_lines
+        output_file.close()
+        print(u, v)
+
+        image = cv2.imread(f'/home/dhlee/meissa/RS-aware-differential-SfM/deepmatching_1.2.2/deepmatching_1.2.2_c++/{dir}/' + img_list[i])
+        height, width, channels = image.shape
+        window_name = 'Image'
+
+        if (u > 0 and v > 0):
+            start_point = (0, height)
+            end_point = (int(u), height - int(v))
+        elif (u > 0 and v <= 0):
+            start_point = (0, height)
+            end_point = (int(u), -1*int(v))
+        elif (u <= 0 and v > 0):
+            start_point = (width, height)
+            end_point = (width + int(u), height - int(v))
+        elif (u <= 0 and v <= 0): 
+            start_point = (width, 0)
+            end_point = (width + int(u), -1*int(v))
+
+        direction = angle_to(start_point, (u,v))
+        print(direction)
+        lines = [str(u), str(v), str(direction)]
+        with open(f"of_{name}.txt", "w") as f:
+            f.write('\n'.join(lines))
+
+        color = (0, 255, 0)
+        thickness = 9
+        image = cv2.arrowedLine(image, start_point, end_point,
+                                            color, thickness)
+        cv2.imwrite(f'arrow_{name}.JPG', image)
+
+if __name__=='__main__':
+    multi_image_visual(args.dir, args.num_imgs)
